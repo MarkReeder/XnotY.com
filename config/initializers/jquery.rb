752f214c76795cd023e62a5a73f973c7e4f25d53
acechase <acechase@gmail.com>
2012-09-08 16:04:11 -0700

    re-generating the skeleton app

--- config/initializers/jquery.rb
+++
@@ -1,7 +0,0 @@
-ActionView::Helpers::AssetTagHelper.register_javascript_expansion :jquery => ['rails']
-
-ActiveSupport.on_load(:action_view) do
-  ActiveSupport.on_load(:after_initialize) do
-    ActionView::Helpers::AssetTagHelper::register_javascript_expansion :defaults => ['rails']
-  end
-end
--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -2,6 +2,6 @@
 
 # Your secret key for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random, 
+# Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Rails.application.config.secret_token = '9e2e5c6cf5a4624ec93fdbbd52f2cc9b19276777cfa7456d036dc2296cd779ad3501363bc8655b0f0c455f696acbc6ffb94c1e6eb64ac35dfd69dd53ff0da32b'
+XnotY::Application.config.secret_token = '4c326f44ff14f0a6ae59b65485e3d598ab61637f2530e84ad3f97844fed0d2688b1d9caa934c8858e123eb1b5480a857594e53c653c5eb63df998fc9e70e5c37'
