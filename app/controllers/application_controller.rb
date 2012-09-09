7eba98bb34783a9ed0177b5d4e44e8eaa4b194f8
acechase <acechase@gmail.com>
2012-09-08 18:35:13 -0700

    fix misspelling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  inlcude Authentication
+  include Authentication
   protect_from_forgery
   before_filter :require_authentication
   before_filter :set_current_user
