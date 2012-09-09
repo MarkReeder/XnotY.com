9a5e92983c214dc4fd53908b9d2cea3c86a909b2
acechase <acechase@gmail.com>
2012-09-08 23:37:25 -0700

    add location and time to a suggestion

--- app/models/suggestion.rb
+++ app/models/suggestion.rb
@@ -2,6 +2,8 @@ class Suggestion
   include Mongoid::Document
   include Mongoid::Timestamps
   field :description, type: String
+  field :location, type: String
+  field :time, type: DateTime
   
   has_many :responses
   belongs_to :user
