f70eae474f4c4f4a1e10879172c2a61f40eafcdf
Ryan Jadhav <ryan.jadhav@gmail.com>
2012-09-09 00:40:05 -0700

    success and fail paths for clicking create event title button

--- app/assets/javascripts/xnoty.js
+++ app/assets/javascripts/xnoty.js
@@ -9,16 +9,19 @@ jQuery(document).ready(function() {
 
   $(document).on('click', '.button-create-event', function() {
     var eventTitle = $('#createEventTitle').val();
-
     $.post('/events.json', {
       'title': eventTitle
-    }, function() {
+    }).done(function() {
+      $('#pageContent').html(Hogan.compile($('#FacebookSendTemplate').html()).render());
+    }).fail(function() {
       $('#pageContent').html(Hogan.compile($('#FacebookConnectTemplate').html()).render());
     });
   });
 
   $(document).on('click', '.button-facebook-connect', function() {
-    XnotY.Facebook.login();
+    XnotY.Facebook.login(function() {
+     $('#pageContent').html(Hogan.compile($('#FacebookSendTemplate').html()).render());
+    });
   });
 
 });
