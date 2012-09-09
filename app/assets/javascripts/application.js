2137b235ee32a22346cdf1e5bbd8929a50d39bd7
Ryan Jadhav <ryan.jadhav@gmail.com>
2012-09-08 21:03:41 -0700

    facebook fix and basic buttons

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,15 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
+//= require _xnoty
 //= require jquery
 //= require jquery_ujs
 //= require foundation
+//= require tinypubsub
+//= require facebook_init
+//= require facebook
+//= require geolocation
+//= require xnoty
 //= require_tree .
+
+
---
+++ app/assets/javascripts/xnoty.js
@@ -0,0 +1,7 @@
+jQuery(document).ready(function() {
+
+  $(document).on('click', '.button-facebook-connect', function() {
+    XnotY.Facebook.login();
+  });
+
+});
